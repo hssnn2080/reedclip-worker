8375e5beb14f724a42c85e6258bc3a5cb645a0c1
hssnn2080 <hssnn2080@users.noreply.github.com>
2026-08-01 05:44:35 +0600

    Fix KeyError R2_ENDPOINT by hardcoding it because it was missing from Endpoint config

--- server.py
+++ server.py
@@ -34,7 +34,7 @@ def process_job(payload):
             print(f"Generated clip at {final_clip}, uploading to R2...")
             # Upload to R2
             s3 = boto3.client('s3', 
-                endpoint_url=os.environ['R2_ENDPOINT'], 
+                endpoint_url="https://a24f98f842e36f14eafe06a5b0f27b91.r2.cloudflarestorage.com", 
                 aws_access_key_id=os.environ['R2_ACCESS_KEY_ID'], 
                 aws_secret_access_key=os.environ['R2_SECRET_ACCESS_KEY']
             )
